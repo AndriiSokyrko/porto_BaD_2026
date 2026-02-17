908cf8cae749b64c9884319a2a5d330811095c11
Andrii Sokyrko <sokyrko.andrii@gmail.com>
2026-02-17 14:52:37 +0200

    Total length 342

--- index.js
+++ index.js
@@ -3,37 +3,48 @@ import fs from 'fs/promises';
 const text = await fs.readFile('source.txt', 'utf-8');
 const arr = text.split('\n');
 const lenArr = arr.length
+let result = []
 
-const resultPuzzle = arr.reduce((acc, item, ind) => {
+function sub(item, temp) {
+    let x = 0
     let currItem = item
-    let temp = []
-    temp.push(item)
-    for (let x = 0; x < lenArr; x++) {
+    for (x; x < lenArr; x++) {
         if (currItem.slice(-2) === arr[x].slice(0, 2)) {
-            //не знаю як рахувати - з повторюючими чи без повторюючих чисел
-            // temp.push(arr[x].slice(2))
-            temp.push(arr[x])
-            currItem = arr[x]
-            for (let y = 0; y < lenArr; y++) {
-                if (currItem.slice(-2) === arr[y].slice(0, 2)) {
-                    if(temp.indexOf(arr[y])===-1){
-                        // temp.push(arr[y].slice(2))
-                        temp.push(arr[y])
-                        currItem = arr[y]
-                    }
-                }
+            if (!temp.includes(arr[x])) {
+                currItem = arr[x]
+                temp.push(arr[x])
+                sub(currItem, temp)
             }
         }
     }
-    if (temp.length > 1) temp.push(currItem)
+    return temp
+}
 
-    return acc.length < temp.length ? temp : acc
-}, [])
+function resultPuzzle() {
+    return arr.reduce((acc, item) => {
+        let currItem = item
+        let temp = [item]
+        temp = sub(item, temp)
+        return result.length < temp.length ? temp : result
+    }, [])
 
-await fs.writeFile(
-    'puzzle.json',
-    JSON.stringify(resultPuzzle ),
-    'utf-8'
-);
-let result =resultPuzzle.join("")
-console.log(result  , result.length )
+}
+
+function print() {
+    let result = resultPuzzle()
+    let len = result.length
+    let finish = [result[0]]
+    let x = 0
+    for (x; x < len; x++) {
+        if (x > 0 && x < len - 1) {
+            finish.push(result[x].slice(-4))
+        }
+    }
+    finish.push(result[x])
+    finish = result.join("")
+    console.log('Length of the puzzle chain: ',finish.length)
+    console.log(finish)
+}
+
+print()
+
